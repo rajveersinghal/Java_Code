d87a5a63c1435614b7f33e11425ebb0560fae1f7
RAJVEER SINGHAL <130908831+rajveersinghal@users.noreply.github.com>
2025-02-05 14:43:46 +0530

    Add files via upload

---
+++ Day 1/EmployeeDetails.java
@@ -0,0 +1,10 @@
+public class EmployeeDetails {
+    public static void main(String[] args) {
+        System.out.println("NAME\tMAIL-ID\t\tEMPLOYEE-CODE\tPHONE");
+        System.out.println("Parul\tparul@cse\tp03161\t\t25764728");
+        System.out.println("Sonia\tsonia@cse\tp03160\t\t25764728");
+        System.out.println("Amit\tamit@cse\tp03162\t\t98765432");
+        System.out.println("Rahul\trahul@cse\tp03163\t\t87654321");
+        System.out.println("Neha\tneha@cse\tp03164\t\t76543210");
+    }
+}
---
+++ Day 1/PrintStatement.java
@@ -0,0 +1,5 @@
+public class PrintStatement {
+    public static void main(String[] args) {
+        System.out.println("\"Can we print '\\' with System.out.println() statement?\"");
+    }
+}
