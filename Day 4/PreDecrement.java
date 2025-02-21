79875b56bb6f497ad6288b4f8ac97d1565de5f53
RAJVEER SINGHAL <130908831+rajveersinghal@users.noreply.github.com>
2025-02-21 22:39:25 +0530

    Add files via upload

---
+++ Day 4/PreDecrement.java
@@ -0,0 +1,8 @@
+public class PreDecrement {
+    public static void main(String[] args) {
+        int n=10;
+        int preDec=--n;
+        System.out.println("Pre decrement: "+preDec);
+        
+    }
+}
---
+++ Day 4/PreIncrement.java
@@ -0,0 +1,7 @@
+public class PreIncrement {
+    public static void main(String[] args) {
+        int n=10;
+        int preIn = ++n;
+        System.out.print("Pre-Increment: "+preIn); 
+    }
+}
